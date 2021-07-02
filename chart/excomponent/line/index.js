1815d17572d394fbcb444567ff3bd81df89f52c3
herzshen <herzshen@tencent.com>
2021-07-02 10:39:50 +0800

    添加折线图

---
+++ chart/excomponent/line/index.js
@@ -0,0 +1,75 @@
+// chart/excomponent/bar/index.js
+import BarHandle from './handle'
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        height: {
+            type: Number,
+            value: 0,
+        },
+        width: {
+            type: Number,
+            value: 0,
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        barHandle: null,
+        letf: 0,
+    },
+    lifetimes: {
+        attached(){
+            this._initChartBox()
+        },
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        _initChartBox(){
+            try {
+                const res = wx.getSystemInfoSync()
+                let { height , width } = res.safeArea
+                this.data.height =  height
+                this.data.width =  width
+                this.setData({ 
+                    height,
+                    width,
+                });
+              } catch (e) {
+                // Do something when catch error
+            }
+            const query = this.createSelectorQuery()
+            query.select('#bar-canvas')
+              .fields({ node: true, size: true })
+              .exec((res) => {
+              
+                const canvas = res[0]
+                // const ctx = canvas.getContext('2d')
+                const ctx = wx.createCanvasContext('bar-canvas', this)
+                console.log('ctx',ctx)
+                const floatctx = wx.createCanvasContext('float-canvas', this)
+                let barHandle = new BarHandle(ctx, floatctx, this)
+                barHandle.setOption({ 
+                    height: canvas.height / 2,
+                    width: canvas.width
+                })
+                this.data.barHandle = barHandle
+                console.log(barHandle.getOption())
+              })
+            
+        },
+        bindTap(event) {
+            // console.log('bindTouchStart', event.detail)
+            let { x, y } = event.detail
+            this.data.barHandle.bindTap(x, y)
+        }
+    }
+})
